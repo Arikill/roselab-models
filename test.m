c932ab302e06e544242dfe3637946c9558aaba2f
ralluri-utah <100333148+ralluri-utah@users.noreply.github.com>
2022-06-02 16:03:59 -0600

    Neuron and Synapse classes working.
    Neuron temporally integrates synaptic responses to generate its output.
    Synapse uses stim triggers to generate its responses on call.

--- test.m
+++ test.m
@@ -1,6 +1,15 @@
 s = Stimulus(1e4, 2200, 'triangular', 20e-3, 2e-3);
 [stim, times, trigs] = s.generateStimulus(1.0, 10, 5);
-excitatory_synapse = Synapse(0.03, 1);
-excitatory_synapse = excitatory_synapse.set(find(trigs)./1e4);
-response = excitatory_synapse.get(times, 'summation', -0.05, 1e4);
-plot(times, response);
+excitatory_synapse = Synapse(1e-9, 0.03);
+excitation = excitatory_synapse.call(trigs, s.fs, 'summation', 0.01);
+inhibitory_synapse = Synapse(1e-9, 0.02);
+inhibition = inhibitory_synapse.call(trigs, s.fs, 'maxima', 0.08);
+neuron = Neuron(14e-12, 0.5e9, -65e-3, -30e-3, -100e-3, -32e-3, 10e-3);
+response = neuron.call(0e-9, excitation, inhibition, 1e4);
+tiledlayout(3, 1)
+nexttile;
+plot(times, response, 'k');
+nexttile;
+plot(times, excitation, 'r', times, -1.*inhibition, 'b');
+nexttile;
+plot(times, stim);
