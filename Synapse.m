4feef328d724a81b0d764c34e0e11914e35e5f24
Sith'ari <ralluri.utah@gmail.com>
2022-06-27 14:09:44 -0600

    updated integration times and types

--- Synapse.m
+++ Synapse.m
@@ -15,30 +15,19 @@ classdef Synapse
             obj.tau = tau;
         end
 
-        function response = call(obj, trigs, fs, integration_type, delay)
+        function response = call(obj, trigs, fs, min_integration_time, integration_tau, delay)
             trig_times = find(trigs)/fs;
             samples = size(trigs, 2);
             times = linspace(0, samples/fs, samples);
             response = zeros(1, size(trigs, 2));
-            if strcmp(integration_type, 'summation')
-                for t = 1: 1: size(trig_times, 2)
-                    exponent = (times > trig_times(t)).*(times - trig_times(t))./obj.tau;
-                    response = response + obj.amp.*exponent.*exp(-1.*exponent + 1);
-                end
-            elseif strcmp(integration_type, 'maxima')
-                for t = 1: 1: size(trig_times, 2)
-                    exponent = (times > trig_times(t)).*(times - trig_times(t))./obj.tau;
-                    response = cat(1, response, obj.amp.*exponent.*exp(-1.*exponent + 1));
-                end
-                response = max(response, [], 1);
-            elseif strcmp(integration_type, 'depression')
-                a = obj.amp;
-                for t = 1: 1: size(trig_times, 2)
-                    exponent = (times > trig_times(t)).*(times - trig_times(t))./obj.tau;
-                    response = response + a.*exponent.*exp(-1.*exponent + 1);
-                    a = a - 0.05*a;
-                end
+            a = obj.amp;
+            factor = mean(diff(trig_times));
+            for t = 1: 1: size(trig_times, 2)
+                exponent = (times > trig_times(t)).*(times - trig_times(t))./obj.tau;
+                response = cat(1, response, a.*exponent.*exp(-1.*exponent + 1));
+                a = a + integration_tau*(factor<min_integration_time)*a;
             end
+            response = max(response, [], 1);
             response = temporalShift(response, delay, fs);
         end
     end
--- test.m
+++ test.m
@@ -1,36 +1,40 @@
 s = Stimulus(1e4, 2200, 'triangular', 20e-3, 2e-3);
-[stim, times, trigs] = s.generateStimulus(1.0, 60, 5);
+[stim, times, trigs] = s.generateStimulus(1.0, 40, 5);
 
+disp("Afferent");
 afferent = {};
 afferent.neuron = Neuron(14e-12, 0.5e9, -65e-3, -30e-3, -100e-3, -55e-3, -20e-3, 0.02);
 afferent.excitatorySynapse = Synapse(1e-9, 0.01);
-afferent.excitation = afferent.excitatorySynapse.call(trigs, s.fs, 'maxima', 0.01);
+afferent.excitation = afferent.excitatorySynapse.call(trigs, s.fs, 0, 0, 0.01);
 afferent.inhibitorySynapse = Synapse(0e-9, 0.01);
-afferent.inhibition = afferent.inhibitorySynapse.call(trigs, s.fs, 'maxima', 0.01);
+afferent.inhibition = afferent.inhibitorySynapse.call(trigs, s.fs, 0, 0, 0.01);
 [afferent.response, afferent.spkTrigs] = afferent.neuron.call(0e-9, afferent.excitation, afferent.inhibition, s.fs);
 
+disp("Relay");
 relayNeuron = {};
 relayNeuron.neuron = Neuron(14e-12, 0.5e9, -65e-3, -30e-3, -100e-3, -55e-3, -20e-3, 0.02);
 relayNeuron.excitatorySynapse = Synapse(1e-9, 0.01);
-relayNeuron.excitation = relayNeuron.excitatorySynapse.call(afferent.spkTrigs, s.fs, 'maxima', 0.01);
+relayNeuron.excitation = relayNeuron.excitatorySynapse.call(afferent.spkTrigs, s.fs, 0, 0, 0.01);
 relayNeuron.inhibitorySynapse = Synapse(0e-9, 0.01);
-relayNeuron.inhibition = relayNeuron.inhibitorySynapse.call(afferent.spkTrigs, s.fs, 'maxima', 0.01);
+relayNeuron.inhibition = relayNeuron.inhibitorySynapse.call(afferent.spkTrigs, s.fs, 0, 0, 0.01);
 [relayNeuron.response, relayNeuron.spkTrigs] = relayNeuron.neuron.call(0e-9, relayNeuron.excitation, relayNeuron.inhibition, s.fs);
 
+disp("LIN");
 lin = {};
-lin.neuron = Neuron(14e-12, 0.5e9, -65e-3, -30e-3, -100e-3, -55e-3, -20e-3, 0.03);
-lin.excitatorySynapse = Synapse(1e-9, 0.04);
-lin.excitation = lin.excitatorySynapse.call(trigs, s.fs, 'maxima', 0.025);
-lin.inhibitorySynapse = Synapse(1e-9, 0.01);
-lin.inhibition = lin.inhibitorySynapse.call(relayNeuron.spkTrigs, s.fs, 'depression', 0.01);
+lin.neuron = Neuron(14e-12, 0.5e9, -65e-3, -30e-3, -100e-3, -57e-3, -20e-3, 0.03);
+lin.excitatorySynapse = Synapse(1e-9, 0.03);
+lin.excitation = lin.excitatorySynapse.call(trigs, s.fs, 1/30, 0.03, 0.035);
+lin.inhibitorySynapse = Synapse(0.8e-9, 0.01);
+lin.inhibition = lin.inhibitorySynapse.call(relayNeuron.spkTrigs, s.fs, 1/30, 0.08, 0.01);
 [lin.response, lin.spkTrigs] = lin.neuron.call(0e-9, lin.excitation, lin.inhibition, s.fs);
 
+disp("ICN");
 icn = {};
-icn.neuron = Neuron(14e-12, 0.5e9, -65e-3, -30e-3, -100e-3, -55e-3, -20e-3, 0.03);
-icn.excitatorySynapse = Synapse(1e-9, 0.01);
-icn.excitation = icn.excitatorySynapse.call(trigs, s.fs, 'summation', 0.025);
-icn.inhibitorySynapse = Synapse(1e-9, 0.01);
-icn.inhibition = icn.inhibitorySynapse.call(lin.spkTrigs, s.fs, 'depression', 0.01);
+icn.neuron = Neuron(14e-12, 0.5e9, -65e-3, -30e-3, -100e-3, -57e-3, -20e-3, 0.03);
+icn.excitatorySynapse = Synapse(0.6e-9, 0.01);
+icn.excitation = icn.excitatorySynapse.call(trigs, s.fs, 1/30, 0.1, 0.055);
+icn.inhibitorySynapse = Synapse(1e-9, 0.015);
+icn.inhibition = icn.inhibitorySynapse.call(lin.spkTrigs, s.fs, 1/30, -0.05, 0.01);
 [icn.response, icn.spkTrigs] = icn.neuron.call(0e-9, icn.excitation, icn.inhibition, s.fs);
 
 figure();
