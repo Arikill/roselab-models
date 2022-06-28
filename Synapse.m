e2b62972ff518db1a896abddb0cb8fef82f95a5a
Sith'ari <ralluri.utah@gmail.com>
2022-06-28 15:09:14 -0600

    Code annotation.

--- Synapse.m
+++ Synapse.m
@@ -1,29 +1,54 @@
 classdef Synapse
-    %UNTITLED2 Summary of this class goes here
-    %   Detailed explanation goes here
+    %Synapse -> Mathematically formulated as an alpha function.
+    %   Formulating the synapse as an alpha functions has several
+    %   advantages: 1) It is close to the synaptic input timecourses as
+    %   seen by the soma of a neuron. 2) Only 3 parameters control the
+    %   timecourse of alpha function, a) amplitude (amp) or gain, b)
+    %   delay(td) or response start and c) timeconstant (tau) of the
+    %   response.
+    %   The alpha function is defined as follows:
+    %   If t > td:
+    %   alpha(t) = amp*((t - td)/tau)*exp(-1*((t - td)/tau) + 1)
+    %   else if t < td:
+    %   alpha(t) = 0
+    %   The function defined as above has its peak at (td + tau)
 
     properties
-        amp
-        tau
+        amp % amplitude (S)
+        tau % timeconstant (secs)
     end
 
     methods
         function obj = Synapse(amp, tau)
-            %UNTITLED2 Construct an instance of this class
-            %   Detailed explanation goes here
+            %Synapse Constructor accepts two parameters as input:
+            %   amplitude (amp) and timeconstant (tau)
+            %   This function or method creates an object called synapse
+            %   defined by its amplitude and timeconstant.
             obj.amp = amp;
             obj.tau = tau;
         end
 
-        function response = call(obj, trigs, fs, min_integration_time, integration_tau, delay)
-            trig_times = find(trigs)/fs;
+        function response = call(obj, input_spikes, fs, min_integration_time, integration_tau, delay)
+            %call computes the output of the synapse when inputs
+            %(input_spikes) temporally arrive at the synapse.
+            % fs is the sampling rate at which the entire system or
+            % simulation is sampled. This sampling rate allows us to
+            % transform from temporal reality to simulation space i.e.,
+            % time to samples.
+            % min_integration_time is the minimum time between input_spikes
+            % at which to temporal overlap proess like summation or
+            % depression start to shape the response.
+            % integration_tau defines the timeconstant of temporal overlap
+            % processes (+ve for summation, -ve for depression and 0 for
+            % neither)
+            input_spike_times = find(input_spikes)/fs;
             samples = size(trigs, 2);
             times = linspace(0, samples/fs, samples);
             response = zeros(1, size(trigs, 2));
             a = obj.amp;
-            factor = mean(diff(trig_times));
-            for t = 1: 1: size(trig_times, 2)
-                exponent = (times > trig_times(t)).*(times - trig_times(t))./obj.tau;
+            factor = mean(diff(input_spike_times));
+            for t = 1: 1: size(input_spike_times, 2)
+                exponent = (times > input_spike_times(t)).*(times - input_spike_times(t))./obj.tau;
                 response = cat(1, response, a.*exponent.*exp(-1.*exponent + 1));
                 a = a + integration_tau*(factor<min_integration_time)*a;
             end
